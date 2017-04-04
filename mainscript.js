c490935620f8407675b6c4f915d64f17d9f9dc2b
bnewbould <benjamin.newbould@my.jcu.edu.au>
2017-04-04 15:52:29 +1000

    Added Javascript, Call to Action
    -Changed all navigation links to buttons powered by javascript
    -Added call to action in the site proper
    -Modified CSS

---
+++ mainscript.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function goToHome(){
+	"use strict";
+	window.location.assign("home.html");
+}
+function goToAbout(){
+	"use strict";
+	window.location.assign("about.html");
+}
+
+function goToGuidelines(){
+	"use strict";
+	window.location.assign("guidelines.html");
+}
+
+function goToCompLeague(){
+	"use strict";
+	window.location.assign("league.html");
+}
+
+function goToLinks(){
+	"use strict";
+	window.location.assign("links.html");
+}
+
+function goToSignUp(){
+	"use strict";
+	window.location.assign("signup.html");
+}
---
+++ splashscript.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function enterSite() {
+	window.location.assign("home.html");
+}
+
+function signUp() {
+	window.location.assign("signup.html");
+}
